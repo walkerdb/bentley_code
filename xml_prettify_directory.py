c533fb4022f2bd6e2834210aab4f8386ea4236b8
Walker Boyle <wboyle13@gmail.com>
2015-05-27 00:13:04 -0400

    remove bentley-specific filepaths

--- xml_prettify_directory.py
+++ xml_prettify_directory.py
@@ -10,7 +10,3 @@ def prettify_xml_in_directory(input_directory, output_directory):
             xml = etree.parse(join(input_directory, filename), parser)
             with open(output_directory + filename, mode='w') as f:
                 f.write(etree.tostring(xml, pretty_print=True))
-
-if __name__ == "__main__":
-    input_directory = 'S:/Curation/Projects/Mellon/ArchivesSpace/ATeam_Migration/EADs/Real_Masters_all'
-    output_directory = 'S:/Curation/Student Work/Walker Boyle/Test environment/source files/EADs/Prettified/'
