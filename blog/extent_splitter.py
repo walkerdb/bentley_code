95f8468c7d88837b477ce6a72d9dbdb76e3c1583
walkerdb <wboyle13@gmail.com>
2015-05-29 15:57:10 -0400

    Further blog edits

--- blog/extent_splitter.py
+++ blog/extent_splitter.py
@@ -1,2 +1,10 @@
-def split_extents(raw_extent_text):
-	return ["4 linear feet", "1 oversize volume"]
+import re
+
+def split_extents(extent_text):
+	regex_to_split_by = r"\,|\[|\(| and | in " # splits by ",", "[", "(", " and ", and " in "
+	extents = filter(None, re.split(regex_to_split_by, extent_text))
+	extents = ["(" + extent if extent.endswith(")") else extent for extent in extents]
+	extents = ["[" + extent if extent.endswith("]") else extent for extent in extents]
+	extents = [extent.strip(" ") for extent in extents]
+
+	return extents
--- blog/tests.py
+++ blog/tests.py
@@ -1,40 +1,56 @@
+import unittest
 from extent_splitter import split_extents
 
-
-
-from unittest import TestCase
-
-class TestExtentSplitter(TestCase):
-    # "setUp" is a special reserved function that is used to define any
-    # variables you will be using throughout the test. It runs before
-    # anything else in the class.
+class TestExtentSplitter(unittest.TestCase):
 
     def setUp(self):
-        self.basic_extent_raw_text = "4 linear feet and 1 oversize volume."
-        self.basic_extent_target_output = ["4 linear feet", "1 oversize volume"]
-
-        self.extent_with_commas = "3 linear ft., 1 oversize volume, and 5 motion picture reels"
+        self.extent_1_raw_text = "4 linear feet and 1 oversize volume"
+        self.extent_1_target_output = ["4 linear feet", "1 oversize volume"]
+        self.extent_2_raw_text = "1 oversize volume and 5 motion picture reels"
+        self.extent_2_target_output = ["1 oversize volume", "5 motion picture reels"]
+        self.extent_with_commas_raw_text = "3 linear ft., 1 oversize volume, and 5 motion picture reels"
         self.extent_with_commas_target_output = ["3 linear ft.", "1 oversize volume", "5 motion picture reels"]
+        self.extent_in_parentheses = "3 linear ft., 1 oversize volume, and 5 motion picture reels (in 4 boxes)"
+        self.extent_in_parentheses_target_output = ["3 linear ft.", "1 oversize volume", "5 motion picture reels", "(in 4 boxes)"]
+        self.extent_in_brackets = "256 MB [150 diskettes]"
+        self.extent_in_brackets_target_output = ["256 MB", "[150 diskettes]"]
+        self.extent_with_in_raw_text = "26 linear feet in 27 boxes"
+        self.extent_with_in_target_output = ["26 linear feet", "27 boxes"]
+        self.extent_with_whitespace = "4 linear feet "
+        self.extent_with_whitespace_target_output = ["4 linear feet"]
 
-    # Now for the tests. We will define each discrete test in its own
-    # function. You want to be as specific as possible when creating test
-    # names - this is really invaluable when figuring out exactly which
-    # tests are failing, and what was being tested when they failed.
+    def test_split_first_two_element_extent_string(self):
+        split_extent = split_extents(self.extent_1_raw_text)
+        self.assertEquals(split_extent, self.extent_1_target_output)
 
-    # All tests need to start with "test" for the class to run properly
-
-    def test_split_basic_two_element_extent_string(self):
-        split_extent = split_extents(self.basic_extent_raw_text)
-
-        # there are a number of built-in assert functions that test a
-        # variety of conditions. We'll use assertEquals, which is the
-        # equivalent of the assert statement we wrote above, just with
-        # a much more semantically helpful name
-        self.assertEquals(split_extent, self.basic_extent_target_output)
+    def test_split_second_two_element_extent_string(self):
+        split_extent = split_extents(self.extent_2_raw_text)
+        self.assertEquals(split_extent, self.extent_2_target_output)
 
     def test_split_extent_with_commas(self):
-        split_extent = split_extents(self.extent_with_commas)
+        split_extent = split_extents(self.extent_with_commas_raw_text)
         self.assertEquals(split_extent, self.extent_with_commas_target_output)
+
+    # When there is a parenthetical extent, this should be split off as well, but should retain parentheses
+    def test_split_parenthetical_extent(self):
+        split_extent = split_extents(self.extent_in_parentheses)
+        self.assertEquals(split_extent, self.extent_in_parentheses_target_output)
+
+    # Same with extents in brackets
+    def test_split_bracketed_extent(self):
+        split_extent = split_extents(self.extent_in_brackets)
+        self.assertEquals(split_extent, self.extent_in_brackets_target_output)
+
+    # Ensure trailing whitespace is removed
+    def test_whitespace_removal(self):
+        split_extent = split_extents(self.extent_with_whitespace)
+        self.assertEquals(split_extent, self.extent_with_whitespace_target_output)
+
+    # Ensure extents are also split by the " in " keyword
+    def test_split_extent_by_in(self):
+        split_extent = split_extents(self.extent_with_in_raw_text)
+        self.assertEquals(split_extent, self.extent_with_in_target_output)
+
 
 if __name__ == "__main__":
     unittest.main()
