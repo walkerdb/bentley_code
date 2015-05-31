d026f15aa6901fece3da5d9667b6a9e7c49d3fff
Walker Boyle <wboyle13@gmail.com>
2015-05-31 16:55:14 -0400

    Blog post updates

--- blog/TDD_post/extent_splitter.py
+++ blog/TDD_post/extent_splitter.py
@@ -1,10 +1,16 @@
 import re
 
 def split_extents(extent_text):
-	regex_to_split_by = r"\,|\[|\(| and | in " # splits by ",", "[", "(", " and ", and " in "
-	extents = filter(None, re.split(regex_to_split_by, extent_text))
-	extents = ["(" + extent if extent.endswith(")") else extent for extent in extents]
-	extents = ["[" + extent if extent.endswith("]") else extent for extent in extents]
-	extents = [extent.strip(" ") for extent in extents]
+    # splits by " and ", " in ", and each of the following characters: ,([
+    regex_to_split_by = re.compile(r"\,|\[|\(| and | in ")
+    extent_list = filter(None, re.split(regex_to_split_by, extent_text))
 
-	return extents
+    # the re.split() function removes the characters it splits by, so if we want to 
+    # preserve the opening parentheses and brackets, we need to add those back
+    extent_list = ["(" + extent if extent.endswith(")") else extent for extent in extent_list]
+    extent_list = ["[" + extent if extent.endswith("]") else extent for extent in extent_list]
+
+    # removing leading and trailing whitespace using the built-in strip() function
+    extent_list = [extent.strip(" ") for extent in extent_list]
+
+    return extent_list
