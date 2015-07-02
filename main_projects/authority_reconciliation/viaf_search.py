91941b179c1ec1c63620aabff287c1ea03358f4b
walkerdb <wboyle13@gmail.com>
2015-07-02 16:55:51 -0400

    more functionality. Now grabs header straight from lc link.

--- main_projects/authority_reconciliation/viaf_search.py
+++ main_projects/authority_reconciliation/viaf_search.py
@@ -1,15 +1,51 @@
 from urllib2 import urlopen, quote
 import csv
 import time
+from bs4 import BeautifulSoup
+
+from lxml import etree
+from tqdm import tqdm
 
 search_types = ["geographicNames", "personalNames", "corporateNames"]
 
 
 def get_auth_id_from_api(target_api, auth_source, search_term, search_type):
 	query = create_query(target_api, auth_source, search_term, search_type)
-	print(query)
 	response = urlopen(query).read()
-	print(response)
+	heading, lc_address = get_auth_data(response)
+
+	with open("geognames_with_ids.csv", mode="ab") as f:
+		writer = csv.writer(f)
+		row = [search_term, heading, lc_address]
+		writer.writerow(row)
+
+
+def get_auth_data(response):
+	lc_template = "http://id.loc.gov/authorities/names/{0}.html"
+	lc_address = ""
+	heading = ""
+	tree = etree.fromstring(response)
+	results = tree.xpath("//*[local-name()='record']")
+	if len(results) > 0:
+		primary_result = results[0]
+		sources = primary_result.xpath("//*[local-name()='mainHeadingEl']/*[local-name()='id']")
+		for source in sources:
+			if "LC|" in source.text:
+				lc_address = lc_template.format(source.text.split("|")[1].replace(" ", ""))
+				heading = get_lc_heading(lc_address)
+				break
+
+	return heading, lc_address
+
+def get_lc_heading(lc_address):
+	try:
+		response = urlopen(lc_address).read()
+		soup = BeautifulSoup(response)
+		header = soup.h1.text.encode("utf-8")
+		return header
+	except:
+		return ""
+
 
 
 def create_query(target_api, auth_source, search_term, search_type=""):
@@ -18,7 +54,6 @@ def create_query(target_api, auth_source, search_term, search_type=""):
 
 	if "viaf" in target_api:
 		search_url_template = 'http://viaf.org/viaf/search/viaf?query=local.{0}+all+{1}+and+local.sources+any+{2}&sortKeys=holdingscount&httpAccept=application/xml'
-
 		return search_url_template.format(search_type, search_term, auth_source)
 
 	elif "lc" in target_api:
@@ -29,7 +64,8 @@ def create_query(target_api, auth_source, search_term, search_type=""):
 if __name__ == "__main__":
 	with open("geognames.csv") as f:
 		reader = csv.reader(f)
-		for location in reader:
-			get_auth_id_from_api("viaf", "lc", location[0], search_type="geographicNames")
+		for location in tqdm(list(reader)):
+			if "--" not in location[0]:
+				get_auth_id_from_api("viaf", "lc", location[0], search_type="geographicNames")
 
-			time.sleep(2)
+				time.sleep(.1)
