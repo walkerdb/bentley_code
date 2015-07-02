37198eeade873c086a6bd92ecf91714a53d4003b
walkerdb <wboyle13@gmail.com>
2015-07-02 14:42:58 -0400

    early abstraction. Some infrastructure for iterating through CSVs

--- main_projects/authority_reconciliation/viaf_search.py
+++ main_projects/authority_reconciliation/viaf_search.py
@@ -1,21 +1,35 @@
 from urllib2 import urlopen, quote
-import httplib
-from collections import namedtuple
+import csv
+import time
 
-search_url_template = 'http://viaf.org/viaf/search/viaf?query=local.{0}+all+{1}+and+local.sources+any+{2}&sortKeys=holdingscount&httpAccept=application/xml'
+search_types = ["geographicNames", "personalNames", "corporateNames"]
 
-SearchTypes = namedtuple("SearchTypes", ["geogname", "corpname", "persname"])
-search_types = SearchTypes(
-	geogname="geographicNames",
-	persname="personalNames",
-	corpname="corporateNames"
-)
 
-authority_source = quote('"lc"')
-search_term = quote('"Zeeland (Mich.)"')
-query = search_url_template.format(search_types.geogname, search_term, authority_source)
-print(query)
+def get_auth_id_from_api(target_api, auth_source, search_term, search_type):
+	query = create_query(target_api, auth_source, search_term, search_type)
+	print(query)
+	response = urlopen(query).read()
+	print(response)
 
-data = urlopen(query)
 
-print(data.read())
+def create_query(target_api, auth_source, search_term, search_type=""):
+	auth_source = quote('"{}"'.format(auth_source))
+	search_term = quote('"{}"'.format(search_term))
+
+	if "viaf" in target_api:
+		search_url_template = 'http://viaf.org/viaf/search/viaf?query=local.{0}+all+{1}+and+local.sources+any+{2}&sortKeys=holdingscount&httpAccept=application/xml'
+
+		return search_url_template.format(search_type, search_term, auth_source)
+
+	elif "lc" in target_api:
+		pass
+
+	# add more API cases here
+
+if __name__ == "__main__":
+	with open("geognames.csv") as f:
+		reader = csv.reader(f)
+		for location in reader:
+			get_auth_id_from_api("viaf", "lc", location[0], search_type="geographicNames")
+
+			time.sleep(2)
