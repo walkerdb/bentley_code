fa3c6bd676bf0e9c2b98a1c00c65b05f53944ddc
walkerdb <wboyle13@gmail.com>
2015-07-06 15:39:07 -0400

    formatting

--- main_projects/authority_reconciliation/false_positive_check.py
+++ main_projects/authority_reconciliation/false_positive_check.py
@@ -2,6 +2,7 @@ import csv
 
 from fuzzywuzzy import fuzz
 
+
 def iterate_through_csv(input_filename, output_filename):
 
     new_auth_data = []
@@ -17,9 +18,11 @@ def iterate_through_csv(input_filename, output_filename):
         writer = csv. writer(f)
         writer.writerows(new_auth_data)
 
+
 def is_same_entity(original_term, returned_term):
     similarity = fuzz.token_sort_ratio(original_term, returned_term)
     return similarity > 95
+
 
 if __name__ == "__main__":
     input_filename = "geognames_with_ids.csv"
