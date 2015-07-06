688d7b4141353037b62ac5f413e494549c55e67b
walkerdb <wboyle13@gmail.com>
2015-07-06 14:10:18 -0400

    more auth code

--- one-off scripts/prettifydirectory.py
+++ one-off scripts/prettifydirectory.py
@@ -17,4 +17,4 @@ def prettify_xml_in_directory(input_dir, output_dir):
 if __name__ == "__main__":
     input_directory = r'C:\Users\wboyle\PycharmProjects\vandura\Real_Masters_all'
     output_directory = r'c:\Users\wboyle\PycharmProjects\bentley_code\main_projects\authority_reconciliation\output_cleaned'
-    remove_newline_from_end_of_file(input_directory, output_directory)
+    prettify_xml_in_directory(input_directory, output_directory)
