1f33decdecf888f93e12cc1d48d1dbfca37ebbbb
walkerdb <wboyle13@gmail.com>
2015-07-06 15:19:04 -0400

    finish encodinganalog discovery code

--- one-off scripts/get_extent_encodinganalogs.py
+++ one-off scripts/get_extent_encodinganalogs.py
@@ -1,11 +1,25 @@
 import csv
 import os
+from pprint import pprint
 
 from lxml import etree
+from tqdm import tqdm
 
-input_dir = '/Users/BHLStaff/PycharmProjects/vandura/Real_Masters_all'
 
-files = [ead for ead in os.listdir(input_dir) if ead.endswith(".xml")]
+def main(input_dir):
+	files = [ead for ead in os.listdir(input_dir) if ead.endswith(".xml")]
 
-for ead in files:
-	pass
+	analogs = {}
+
+	for ead in tqdm(files):
+		tree = etree.parse(os.path.join(input_dir, ead))
+		extents = tree.xpath("//extent")
+		for extent in extents:
+			analog = extent.attrib.get("encodinganalog", "")
+			analogs[analog] = analogs.get(analog, 0) + 1
+
+	pprint(analogs)
+
+if __name__ == "__main__":
+	input_dir = r'C:\Users\wboyle\PycharmProjects\vandura\Real_Masters_all'
+	main(input_dir)
