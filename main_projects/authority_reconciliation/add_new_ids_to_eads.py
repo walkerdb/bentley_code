6487b850dfe0cacb56af46060bd91ae6596be04b
walkerdb <wboyle13@gmail.com>
2015-07-06 14:09:31 -0400

    more auth code

---
+++ main_projects/authority_reconciliation/add_new_ids_to_eads.py
@@ -0,0 +1,30 @@
+import csv
+import os
+
+from lxml import etree
+from tqdm import tqdm
+
+def write_ids_to_ead(auth_dict, input_dir, output_dir):
+	tags = ['subject', 'corpname', 'geogname', 'persname', 'genreform', 'famname']
+	eads = [ead for ead in os.listdir(input_dir) if ead.endswith(".xml")]
+
+	for ead in tqdm(eads):
+		tree = etree.parse(os.path.join(input_dir, ead))
+
+		for controlaccess_child in tree.xpath('//controlaccess/*'):
+			if any([controlaccess_child.tag in tag for tag in tags]):
+				if controlaccess_child.text in auth_dict:
+					controlaccess_child.attrib['authfilenumber'] = auth_dict[controlaccess_child.text]
+
+		with open(os.path.join(output_dir, ead), mode="w") as f:
+			f.write(etree.tostring(tree))
+
+
+if __name__ == "__main__":
+	with open("geogname_id_dict.txt", mode="r") as f:
+		auth_dict = eval(f.read())
+
+	input_dir = r'C:\Users\wboyle\PycharmProjects\vandura\Real_Masters_all'
+	output_dir = r'c:\Users\wboyle\PycharmProjects\bentley_code\main_projects\authority_reconciliation\output'
+
+	write_ids_to_ead(auth_dict, input_dir, output_dir)
--- one-off scripts/prettifydirectory.py
+++ one-off scripts/prettifydirectory.py
@@ -6,16 +6,15 @@ from tqdm import tqdm
 
 
 def prettify_xml_in_directory(input_dir, output_dir):
-	parser = etree.XMLParser(remove_blank_text=True)
-	for filename in tqdm(os.listdir(input_dir), desc="Prettify progress", leave=True):
-		if filename.endswith(".xml"):
-			xml = etree.parse(join(input_dir, filename), parser)
-			with open(join(output_dir, filename), mode='w') as f:
-				f.write(etree.tostring(xml, pretty_print=True, xml_declaration=True, encoding="utf-8"))
-
+    parser = etree.XMLParser(remove_blank_text=True)
+    for filename in tqdm(os.listdir(input_dir), desc="Prettify progress", leave=True):
+        if filename.endswith(".xml"):
+            xml = etree.parse(join(input_dir, filename), parser)
+            with open(join(output_dir, filename), mode='w') as f:
+                f.write(etree.tostring(xml, pretty_print=True, xml_declaration=True, encoding="utf-8"))
 
 
 if __name__ == "__main__":
-	input_directory = '/Users/BHLStaff/PycharmProjects/bentley_code/one-off scripts/output'
-	output_directory = '/Users/BHLStaff/PycharmProjects/bentley_code/one-off scripts/output'
-	prettify_xml_in_directory(input_directory, output_directory)
+    input_directory = r'C:\Users\wboyle\PycharmProjects\vandura\Real_Masters_all'
+    output_directory = r'c:\Users\wboyle\PycharmProjects\bentley_code\main_projects\authority_reconciliation\output_cleaned'
+    remove_newline_from_end_of_file(input_directory, output_directory)
