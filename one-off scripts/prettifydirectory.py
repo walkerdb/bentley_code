c32d578af2fbdcd69b1739c43547c96d11d59dff
walkerdb <wboyle13@gmail.com>
2015-07-17 15:15:26 -0400

    remove pretty-print parser (was removing essential spaces between inline tags)

--- one-off scripts/prettifydirectory.py
+++ one-off scripts/prettifydirectory.py
@@ -6,10 +6,10 @@ from tqdm import tqdm
 
 
 def prettify_xml_in_directory(input_dir, output_dir):
-    parser = etree.XMLParser(remove_blank_text=True)
+    # parser = etree.XMLParser(remove_blank_text=True)
     for filename in tqdm(os.listdir(input_dir), desc="Prettify progress", leave=True):
         if filename.endswith(".xml"):
-            xml = etree.parse(join(input_dir, filename), parser)
+            xml = etree.parse(join(input_dir, filename))
             with open(join(output_dir, filename), mode='w') as f:
                 f.write(etree.tostring(xml, pretty_print=True, xml_declaration=True, encoding="utf-8"))
 
