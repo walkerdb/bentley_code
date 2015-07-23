de69a7b0469996495159450dfa0f308ef113201e
walkerdb <wboyle13@gmail.com>
2015-07-23 15:27:35 -0400

    only look for subject tags in controlaccess and origination parent tags

--- one-off scripts/subject_manipulation/grab_subjects.py
+++ one-off scripts/subject_manipulation/grab_subjects.py
@@ -3,31 +3,37 @@ import os
 from os.path import join
 
 from lxml import etree
+from tqdm import tqdm
 
 # change this to to full path to your own ead folder
-path_to_eads = 'path/to/your/eads'
+path_to_eads = r'C:\Users\wboyle\PycharmProjects\vandura\Real_Masters_all'
 
 # edit this list to include any other tags whose text you want to normalize
 controlaccess_tags = ['subject', 'corpname', 'geogname', 'persname', 'genreform', 'famname']
 
 
 def build_master_subject_csv():
-    with open('all_subjects.csv', 'ab') as csvfile:
+    with open('all_subjects.csv', 'wb') as csvfile:
         writer = csv.writer(csvfile, dialect='excel')
+        headers = ['ead', 'controlaccess type', 'controlaccess text', 'controlaccess auth source', 'xpath']
+        writer.writerow(headers)
 
-        for filename in [ead for ead in os.listdir(path_to_eads) if ead.endswith(".xml")]:
+        for filename in tqdm([ead for ead in os.listdir(path_to_eads) if ead.endswith(".xml")]):
             tree = etree.parse(join(path_to_eads, filename))
-            for tag in controlaccess_tags:
-                for result in tree.xpath("//{0}".format(tag)):
-                    if result.text is not None:
-                        text = result.text.encode("utf-8")  # the encode call is necessary for text with special characters
-                        xpath = tree.getpath(result)
+            controlaccess_children = tree.xpath("//controlaccess/*")
+            origination_children = tree.xpath("//origination/*")
+            elements_to_look_through = controlaccess_children + origination_children
 
-                        if 'source' in result.attrib:
-                            writer.writerow([filename, tag, text, result.attrib['source'], xpath])
+            for element in elements_to_look_through:
+                if any([element.tag == controlaccess_tag for controlaccess_tag in controlaccess_tags]):
+                    if element.text is not None:
+                        text = element.text.encode("utf-8")  # the encode call is necessary for text with special characters
+                        xpath = tree.getpath(element)
+
+                        if 'source' in element.attrib:
+                            writer.writerow([filename, element.tag, text, element.attrib['source'], xpath])
                         else:
-                            writer.writerow([filename, tag, text, '', xpath])
+                            writer.writerow([filename, element.tag, text, '', xpath])
 
 if __name__ == "__main__":
     build_master_subject_csv()
-
