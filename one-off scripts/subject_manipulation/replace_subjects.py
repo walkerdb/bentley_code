c389f3412e2357b4d7cdba4b82c6653fac736d40
walkerdb <wboyle13@gmail.com>
2015-07-23 16:17:22 -0400

    updates to replace_subjects

--- one-off scripts/subject_manipulation/replace_subjects.py
+++ one-off scripts/subject_manipulation/replace_subjects.py
@@ -1,36 +1,41 @@
 import csv
 from os import path
 
+# You'll need to install the two modules below.
+# Should be as easy as running "pip install lxml" and "pip install tqdm" from the command-line
 from lxml import etree
+from tqdm import tqdm
 
-# change these to your wherever you would like them to be pointing
-input_dir = "path/to/master/eads"
-output_dir = "path/to/output/folder"
-path_to_refined_csv_file = "path/to/csv_data.csv"
+# change these to wherever you would like them to be pointing
+# make sure to create the output folder before running the script, or it will crash
+input_dir = 'path/to/input/ead/directory'
+output_dir = 'path/to/output/directory'
+path_to_refined_csv_file = "path/to/csv/data.csv"
 
 
 def write_refined_text_to_eads():
     with open(path_to_refined_csv_file, mode="r") as f:
         reader = csv.reader(f)
+        next(reader)  # skip the header row
+
         previous_filename = ""
         tree = ""
         edited_filenames = set()
 
-        for filename, tag, text, authority, xpath in reader:
+        for filename, tag, text, authority, xpath in tqdm(list(reader)):
             filepath = path.join(input_dir, filename) if filename not in edited_filenames else path.join(output_dir, filename)
     
-            # only make a new root tree and write the old one to file if the filename listed in the csv has changed
+            # only makes a new root tree and writes the old one to file if the filename listed in the csv has changed
             # this makes things much faster
             if not previous_filename:
                 tree = etree.parse(filepath)
-                print("Working on {0}...".format(filename))
             elif filename != previous_filename:
-                tree.write(path.join(output_dir, previous_filename), pretty_print=True)
+                tree.write(path.join(output_dir, previous_filename), pretty_print=True, xml_declaration=True, encoding="utf-8")
                 tree = etree.parse(filepath)
-                print("Working on {0}...".format(filename))
 
             previous_filename = filename
             replace_subject(tree, filename, text, authority, xpath)
+            edited_filenames.add(filename)
 
 
 def replace_subject(tree, filename, text, authority, xpath):
