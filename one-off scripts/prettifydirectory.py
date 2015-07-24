25de1f46ddc01cf06e41f8241051a670f712680b
walkerdb <wboyle13@gmail.com>
2015-07-24 10:34:01 -0400

    removed double-space removal bits. Too clever; removed valid spaces.

--- one-off scripts/prettifydirectory.py
+++ one-off scripts/prettifydirectory.py
@@ -17,4 +17,4 @@ def prettify_xml_in_directory(input_dir, output_dir):
 if __name__ == "__main__":
     input_directory = r'C:\Users\wboyle\PycharmProjects\bentley_code\one-off scripts\prettyprint_whitespace_fix\output'
     output_directory = r'C:\Users\wboyle\PycharmProjects\vandura\Real_Masters_all'
-    prettify_xml_in_directory(output_directory, output_directory)
+    prettify_xml_in_directory(input_directory, output_directory)
--- one-off scripts/prettyprint_whitespace_fix/test_fix_lost_whitespace.py
+++ one-off scripts/prettyprint_whitespace_fix/test_fix_lost_whitespace.py
@@ -19,9 +19,10 @@ class TestLostWhitespaceFix(unittest.TestCase):
 	def test_item_complicated(self):
 		self.assertEquals(fix("""</title></item><item><title render="italic">"""), """</title></item><item><title render="italic">""")
 
-	def test_doublespaced(self):
-		self.assertEquals(fix("</tag> <tag> </tag>"), "</tag> <tag></tag>")
-		self.assertEquals(fix('<title render="italic">Grutter v. Bollinger, et al</title> and <title render="italic">Gratz and Hamacher v. Bollinger, et al</title>'), '<title render="italic">Grutter v. Bollinger, et al</title> and <title render="italic">Gratz and Hamacher v. Bollinger, et al</title>')
-		self.assertEquals(fix('<unittitle><unitdate type="inclusive" normal="1986-06">February, June 1986</unitdate> <unitdate type="inclusive" normal="1987-08">March, July, August 1987</unitdate> <unitdate type="inclusive" normal="1988">May, Sept 1988</unitdate></unittitle>'), '<unittitle><unitdate type="inclusive" normal="1986-06">February, June 1986</unitdate> <unitdate type="inclusive" normal="1987-08">March, July, August 1987</unitdate> <unitdate type="inclusive" normal="1988">May, Sept 1988</unitdate></unittitle>')
+	# def test_doublespaced(self):
+	# 	self.assertEquals(fix("</tag> <tag> </tag>"), "</tag> <tag></tag>")
+	# 	self.assertEquals(fix('<title render="italic">Grutter v. Bollinger, et al</title> and <title render="italic">Gratz and Hamacher v. Bollinger, et al</title>'), '<title render="italic">Grutter v. Bollinger, et al</title> and <title render="italic">Gratz and Hamacher v. Bollinger, et al</title>')
+	# 	self.assertEquals(fix('<unittitle><unitdate type="inclusive" normal="1986-06">February, June 1986</unitdate> <unitdate type="inclusive" normal="1987-08">March, July, August 1987</unitdate> <unitdate type="inclusive" normal="1988">May, Sept 1988</unitdate></unittitle>'), '<unittitle><unitdate type="inclusive" normal="1986-06">February, June 1986</unitdate> <unitdate type="inclusive" normal="1987-08">March, July, August 1987</unitdate> <unitdate type="inclusive" normal="1988">May, Sept 1988</unitdate></unittitle>')
+
 if __name__ == "__main__":
 	unittest.main()
