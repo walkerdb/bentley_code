4e01d5ae89e307ea435de5185134a88c9d34ec0c
walkerdb <wboyle13@gmail.com>
2015-07-27 16:08:57 -0400

    modify container range discovery to also output collection and unittitle names

--- one-off scripts/expand_container_ranges/find_container_ranges.py
+++ one-off scripts/expand_container_ranges/find_container_ranges.py
@@ -1,9 +1,16 @@
 from os import listdir, path
 from collections import OrderedDict
 import csv
+import re
 
 from tqdm import tqdm
 from lxml import etree
+
+def remove_tags(tag_string):
+	tag_regex = r"\<.*?\>"
+	tag_string = " ".join(tag_string.split())
+	return re.sub(tag_regex, "", tag_string)
+
 
 def find_container_ranges():
 	input_dir = r'C:\Users\wboyle\PycharmProjects\vandura\Real_Masters_all'
@@ -16,19 +23,33 @@ def find_container_ranges():
 		containers = tree.xpath("//container")
 		boxnum_count_dict = OrderedDict()
 
+		ead_title = tree.xpath("/ead/archdesc/did/unittitle")[0]
+		ead_title = remove_tags(etree.tostring(ead_title))
+
 		for container in containers:
+			sibling_containers = tree.xpath(tree.getpath(container.getparent()) + "/container")
+			sibling_container_count = len(tree.xpath(tree.getpath(container.getparent()) + "/container"))
+			if sibling_container_count > 1:
+				print("Multiple container paths in {0}".format(ead_title))
+
 			if container.text:
 				if "-" in container.text:
 					try:
 						left, right = container.text.split("-")
 						parent_xpath = tree.getpath(container.getparent().getparent().getparent())
 						key = container.text + parent_xpath
+						try:
+							unittitle_text = tree.xpath(tree.getpath(container.getparent().getparent()) + "/did/unittitle")[0]
+							unittitle_text = remove_tags(etree.tostring(unittitle_text))
+						except:
+							unittitle_text = "failed to grab unittitle text"
+							print("unittitle failure")
 
 						# checks to see if it's a valid range. Elements on either side of the "-" must be numbers, and
 						# the first number must be greater than the second
 						if is_number(left) and is_number(right) and not any(ead == forbidden_ead for forbidden_ead in eads_to_skip):
 							if int(left) < int(right):
-								boxnum_count_dict[key] = boxnum_count_dict.get(key, [0, ead, tree.getpath(container), container.text])
+								boxnum_count_dict[key] = boxnum_count_dict.get(key, [0, ead, ead_title, tree.getpath(container), unittitle_text, container.text])
 								boxnum_count_dict[key][0] += 1
 
 					except ValueError:
