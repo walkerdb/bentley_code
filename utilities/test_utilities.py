bf18d1a975a1ac4d2e2002a7b14cc5fdb0e1f293
walkerdb <wboyle13@gmail.com>
2015-08-26 22:35:43 -0400

    restructuring class, new tests

--- utilities/test_utilities.py
+++ utilities/test_utilities.py
@@ -1,21 +1,46 @@
+import os
 import unittest
-from extents_in_unittitle_parens import EAD
+import contextlib
 
 from lxml import etree
+
+from utilities import EAD
 
 
 class TestEADClass(unittest.TestCase):
     def setUp(self):
-        self.ead = EAD()
-        self.ead.set_path("ead.xml")
+        self.ead = EAD(filepath="ead_messy.xml")
 
-    def test_class_setup(self):
-        ead = EAD()
-        ead.set_path("ead.xml")
-        self.assertEquals(ead.path_to_ead, "ead.xml")
+    def test_filename(self):
+        self.assertEquals(self.ead.filename, "ead_messy.xml")
 
     def test_etree_creation(self):
-        self.ead.make_tree()
+        self.assertEquals(etree.tostring(self.ead.tree), etree.tostring(etree.parse("ead_messy.xml")))
 
-        print(self.ead.path_to_ead)
-        self.assertEquals(self.ead.tree, etree.parse("ead.xml"))
+    def test_pretty_printing(self):
+        output_dir = "output"
+        self.ead.prettyprint(output_dir=output_dir)
+        with open(os.path.join(output_dir, self.ead.filename)) as f:
+            ead_prettyprinted = f.read()
+
+        with open("ead_pretty.xml") as f:
+            ideal_output = f.read()
+
+        self.assertEquals(ead_prettyprinted, ideal_output)
+
+        os.remove(os.path.join(output_dir, self.ead.filename))
+
+    def test_edit_and_output(self):
+        new_node = etree.Element("new_node")
+        new_node.text = "new text"
+
+        self.ead.tree.xpath("/ead")[0].append(new_node)
+        self.ead.prettyprint(output_dir="output")
+
+        with open(os.path.join("output", self.ead.filename)) as f:
+            ead_prettyprinted = f.read()
+
+        with open("ead_appended.xml") as f:
+            ideal_output = f.read()
+
+        self.assertEquals(ead_prettyprinted, ideal_output)
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,13 +1,28 @@
+import os
+import re
+
 from lxml import etree
 
 class EAD (object):
 
-    def __init__(self):
-        self.path_to_ead = ""
-        self.tree = ""
-
-    def set_path(self, ead_path):
-        self.path_to_ead = ead_path
-
-    def make_tree(self):
+    def __init__(self, filepath):
+        self.path_to_ead = filepath
+        self.filename = os.path.basename(self.path_to_ead)
         self.tree = etree.parse(self.path_to_ead)
+
+    def prettyprint(self, output_dir):
+        with open(os.path.join(output_dir, self.filename), mode="w") as f:
+            fixed_text = self.__fix_prettyprint_whitespace__(
+                etree.tostring(self.tree, pretty_print=True, xml_declaration=True, encoding="utf-8")
+            )
+            f.write(fixed_text)
+
+    @staticmethod
+    def __fix_prettyprint_whitespace__(raw_text):
+        open_to_close_tag_regex = r'(\<\/.*?\>)(\<[^\/]*?\>)'
+        item_regex = r'(\<\/item\>)\ (\<item\>)'
+
+        text = re.sub(open_to_close_tag_regex, r'\g<1> \g<2>', raw_text)
+        text = re.sub(item_regex, r'\g<1>\g<2>', text)
+
+        return text
