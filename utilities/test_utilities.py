4d290a22eb109a707a6b697453d35bda0d07810f
walkerdb <wboyle13@gmail.com>
2015-08-31 11:37:56 -0400

    add EADDir class

--- utilities/test_utilities.py
+++ utilities/test_utilities.py
@@ -1,10 +1,9 @@
 import os
 import unittest
-import contextlib
 
 from lxml import etree
 
-from utilities import EAD
+from utilities import EAD, EADDir
 
 
 class TestEADClass(unittest.TestCase):
@@ -44,3 +43,37 @@ class TestEADClass(unittest.TestCase):
             ideal_output = f.read()
 
         self.assertEquals(ead_prettyprinted, ideal_output)
+
+class TestApplicator(unittest.TestCase):
+    def setUp(self):
+        self.a = EADDir(input_dir=r'C:\Users\wboyle\PycharmProjects\bentley_code\utilities')
+
+    def _method_for_testing_write(self, ead):
+        texts = ead.tree.xpath("//text")
+        for text in texts:
+            text.text = "yo"
+
+    def _method_for_testing_characterize(self, ead):
+        results = []
+        texts = ead.tree.xpath("//text")
+        for text in texts:
+            results.append(text.text)
+        return results
+
+    def test_ead_list(self):
+        self.assertEquals(self.a.ead_files, ['ead_appended.xml', 'ead_messy.xml', 'ead_pretty.xml'])
+
+    def test_characterize_directory(self):
+        intended_results = [['text'], ['text'], ['text']]
+        self.assertEquals(self.a.characterize_dir(function=self._method_for_testing_characterize), intended_results)
+
+    def test_apply_to_directory(self):
+        output_dir = os.path.join(self.a.input_dir, "output")
+        self.a.apply_function_to_dir(function=self._method_for_testing_write, output_dir=output_dir)
+
+        b = EADDir(output_dir)
+        intended_results = [['yo'], ['yo'], ['yo']]
+        self.assertEquals(b.characterize_dir(self._method_for_testing_characterize), intended_results)
+
+        for ead in os.listdir(output_dir):
+            os.remove(os.path.join(output_dir, ead))
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -2,8 +2,10 @@ import os
 import re
 
 from lxml import etree
+from tqdm import tqdm
 
-class EAD (object):
+
+class EAD(object):
 
     def __init__(self, filepath):
         self.path_to_ead = filepath
@@ -26,3 +28,24 @@ class EAD (object):
         text = re.sub(item_regex, r'\g<1>\g<2>', text)
 
         return text
+
+
+class EADDir(object):
+    def __init__(self, input_dir=r'C:\Users\wboyle\PycharmProjects\vandura\Real_Masters_all'):
+        self.input_dir = input_dir
+        self.ead_files = [ead for ead in os.listdir(input_dir) if ead.endswith(".xml")]
+
+    def apply_function_to_dir(self, function, output_dir):
+        for ead_file in tqdm(self.ead_files):
+            ead = EAD(os.path.join(self.input_dir, ead_file))
+            function(ead)
+            ead.prettyprint(output_dir)
+
+    def characterize_dir(self, function):
+        results = []
+        for ead_file in tqdm(self.ead_files):
+            ead = EAD(os.path.join(self.input_dir, ead_file))
+            result = function(ead)
+            results.append(result)
+
+        return results
