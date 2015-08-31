e84b43c811a3c9702213e324f77bfec04f54289a
walkerdb <wboyle13@gmail.com>
2015-08-31 11:32:57 -0400

    initial commit - script to find redundantly nested tags

---
+++ one-off scripts/check_tag_nesting/test_check_nested_tags.py
@@ -0,0 +1,19 @@
+import unittest
+
+from lxml import etree
+
+from utilities.utilities import EAD
+from check_nested_tags import find_nested_tags
+
+class TestCheckNestedTags(unittest.TestCase):
+    def setUp(self):
+        self.positive_test_case_loc = r"C:\Users\wboyle\PycharmProjects\bentley_code\one-off scripts\check_tag_nesting\positive_case_ead.xml"
+        self.negative_test_case_loc = r"C:\Users\wboyle\PycharmProjects\bentley_code\one-off scripts\check_tag_nesting\negative_case_ead.xml"
+
+    def test_positive_match(self):
+        ead = EAD(filepath=self.positive_test_case_loc)
+        self.assertEquals(find_nested_tags(ead), [['positive_case_ead.xml', '/unittitle', 'unittitle']])
+
+    def test_negatie_match(self):
+        ead = EAD(filepath=self.negative_test_case_loc)
+        self.assertEquals(find_nested_tags(ead), [])
