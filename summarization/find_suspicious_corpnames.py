417f7ac5868eb0edee07ea739d4741129af3ad56
walkerdb <wboyle13@gmail.com>
2016-02-05 10:52:52 -0500

    some refactoring

--- summarization/find_suspicious_corpnames.py
+++ summarization/find_suspicious_corpnames.py
@@ -7,7 +7,18 @@ from tqdm import tqdm
 from utilities.ead_utilities.ead_utilities import EADDir, EAD
 
 
-def get_corpnames(ead):
+def main():
+    combined_results = []
+    ead_dir = EADDir()
+    for filename in tqdm(ead_dir.ead_files):
+        ead = EAD(os.path.join(ead_dir.input_dir, filename))
+        combined_results += get_suspicious_corpnames(ead)
+
+    pprint(sorted(sorted(Counter(combined_results).most_common(), key=lambda x: x[0]), key=lambda x: -x[1]))
+
+
+
+def get_suspicious_corpnames(ead):
     results = []
     corpnames = ead.tree.xpath("//corpname")
     disallowed_keywords = ["&amp", "Inc", "Michigan", "University", "United", "(", "Co.", "Ann Arbor"]
@@ -28,10 +39,5 @@ def extract_text(tag):
 
     return " ".join(re.sub(tag_regex, "", text).split()).strip()
 
-combined_results = []
-ead_dir = EADDir()
-for filename in tqdm(ead_dir.ead_files):
-    ead = EAD(os.path.join(ead_dir.input_dir, filename))
-    combined_results += get_corpnames(ead)
-
-pprint(sorted(sorted(Counter(combined_results).most_common(), key=lambda x: x[0][0]), key=lambda x: -x[1]))
+if __name__ == "__main__":
+    main()
