ffca7f6b2785afcd767a91c9c8a3dbceffff710c
walkerdb <wboyle13@gmail.com>
2016-02-16 13:00:49 -0500

    add comment

--- normalization/aspaceify_extents/aspaceify_extents/scripts/extent_grab.py
+++ normalization/aspaceify_extents/aspaceify_extents/scripts/extent_grab.py
@@ -12,6 +12,8 @@ def get_all_extents(input_dir):
     for ead in eads:
         root_tree = etree.parse(path.join(input_dir, ead))
         extents = root_tree.xpath("//extent")
+
+        # only grab data for extent tags that don't represent containers
         extent_list = [[ead, root_tree.getpath(extent), extent.text.encode('utf-8') if extent.text else ""] for extent in extents if extent.get("altrender", "") != "carrier"]
         extents.append(extent_list)
 
