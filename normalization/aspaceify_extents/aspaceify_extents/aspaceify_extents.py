6f60f9b99b81d83fc79ad06fe9776265c680026d
walkerdb <wboyle13@gmail.com>
2016-02-16 12:59:07 -0500

    modify to remove dependency on writing to file

--- normalization/aspaceify_extents/aspaceify_extents/aspaceify_extents.py
+++ normalization/aspaceify_extents/aspaceify_extents/aspaceify_extents.py
@@ -11,68 +11,69 @@ from .scripts.extent_parser import split_into_extents
 from .scripts import etree_editor
 
 
-def main(source="C:/Users/wboyle/PycharmProjects/bentley_code/main_projects/aspaceify_extents/input/all_extents_reversed.csv",
+def main(extent_list,
          path_to_eads="C:/Users/wboyle/PycharmProjects/vandura/Real_Masters_all",
          path_to_output="C:/Users/wboyle/PycharmProjects/bentley_code/main_projects/aspaceify_extents/output"):
 
     edited_filenames = set()
     types = {}
 
-    with open(source, mode="r") as f:
-        reader = csv.reader(f)
-        reader.next()  # skip the header row
+    previous_filename = ""
+    tree = ""
 
-        previous_filename = ""
-        tree = ""
-        for filename, xpath, longform_extent_statement in tqdm(list(reader), desc="Extent parsing progress", leave=True):
+    # initialize errors csv
+    with open("extents_with_errors.csv", mode="ab") as f:
+        pass
 
-            # if this file has already been edited, read it from the output directory, not the input directory
-            filepath = path.join(path_to_eads, filename) if filename not in edited_filenames else path.join(path_to_output, filename)
+    for filename, xpath, longform_extent_statement in tqdm(extent_list, desc="Extent parsing progress", leave=True):
 
-            # only make a new root tree and write the old one to file if the filename has changed
-            if not previous_filename:
-                tree = etree.parse(filepath)
-            elif filename != previous_filename:
-                try:
-                    tree.write(path.join(path_to_output, previous_filename), pretty_print=True)
-                except AttributeError:
-                    print(type(tree) + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                tree = etree.parse(filepath)
-            previous_filename = filename
+        # if this file has already been edited, read it from the output directory, not the input directory
+        filepath = path.join(path_to_eads, filename) if filename not in edited_filenames else path.join(path_to_output, filename)
 
+        # only make a new root tree and write the old one to file if the filename has changed
+        if not previous_filename:
+            tree = etree.parse(filepath)
+        elif filename != previous_filename:
             try:
-                # find the xpath of the extent's parent
-                parent_of_physdesc_xpath = etree_editor.get_parent_node(tree, xpath)
+                tree.write(path.join(path_to_output, previous_filename), pretty_print=True)
+            except AttributeError:
+                print(type(tree) + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            tree = etree.parse(filepath)
+        previous_filename = filename
 
-                physdesc = tree.xpath(xpath)[0].getparent()
-                portion = physdesc.get("portion", "")
+        try:
+            # find the xpath of the extent's parent
+            parent_of_physdesc_xpath = etree_editor.get_parent_node(tree, xpath)
+
+            physdesc = tree.xpath(xpath)[0].getparent()
+            portion = physdesc.get("altrender", "")
 
 
-                # split original extent text into component parts
-                highlevel_extents = split_into_extents(longform_extent_statement)
-                is_multiple_extents = True if len(highlevel_extents) > 1 else False
-                aspace_components = [split_into_aspace_components(extent, physdesc, portion, is_multiple_extents) for extent in highlevel_extents]
+            # split original extent text into component parts
+            highlevel_extents = split_into_extents(longform_extent_statement)
+            is_multiple_extents = True if len(highlevel_extents) > 1 else False
+            aspace_components = [split_into_aspace_components(extent, physdesc, portion, is_multiple_extents) for extent in highlevel_extents]
 
-                # delete the old extent node
-                tree = etree_editor.delete_node(tree, xpath)
+            # delete the old extent node
+            tree = etree_editor.delete_node(tree, xpath)
 
-                # write new extent xml to file
-                tree = etree_editor.write_aspace_extent_tags(filename, tree, parent_of_physdesc_xpath, aspace_components)
+            # write new extent xml to file
+            tree = etree_editor.write_aspace_extent_tags(filename, tree, parent_of_physdesc_xpath, aspace_components)
 
-                edited_filenames.add(filename)
+            edited_filenames.add(filename)
 
-                # temp code to get list of all types in collection
-                for aspace_component in aspace_components:
-                    type_ = aspace_component.type_.strip(" ")
-                    type_ = type_.lstrip("1234567890.- ")
-                    # type_ = type_.strip(""" .;:()-"'""")
-                    types[type_] = types.get(type_, 0) + 1
+            # temp code to get list of all types in collection
+            for aspace_component in aspace_components:
+                type_ = aspace_component.type_.strip(" ")
+                type_ = type_.lstrip("1234567890.- ")
+                # type_ = type_.strip(""" .;:()-"'""")
+                types[type_] = types.get(type_, 0) + 1
 
-            except (IndexError, ValueError) as e:
-                with open("extents_with_errors.csv", mode="ab") as f:
-                    writer = csv.writer(f)
-                    writer.writerow([filename, xpath, longform_extent_statement, str(e)])
-                continue
+        except (IndexError, ValueError) as e:
+            with open("extents_with_errors.csv", mode="ab") as f:
+                writer = csv.writer(f)
+                writer.writerow([filename, xpath, longform_extent_statement, str(e)])
+            continue
 
     # clean up the changes
     prettify_xml_in_directory(input_dir=path_to_output, output_dir=path_to_output)
--- normalization/aspaceify_extents/aspaceify_extents/scripts/extent_grab.py
+++ normalization/aspaceify_extents/aspaceify_extents/scripts/extent_grab.py
@@ -1,31 +1,24 @@
 from os import listdir
 from os import path
-import csv
 
 from lxml import etree
 
 
-def write_extents_to_csv(input_dir, output_dir="."):
+def get_all_extents(input_dir):
     eads = listdir(input_dir)
     eads = [ead for ead in eads if ead.endswith(".xml")]
+    extents = []
 
-    with open(path.join(output_dir, "all_extents.csv"), mode="wb") as f:
-        writer = csv.writer(f)
-        for ead in eads:
-            print(ead)
-            root_tree = etree.parse(path.join(input_dir, ead))
-            extents = root_tree.xpath("//extent")
-            extent_list = [[ead, root_tree.getpath(extent), extent.text.encode('utf-8') if extent.text else ""] for extent in extents]
-            writer.writerows(extent_list)
+    for ead in eads:
+        root_tree = etree.parse(path.join(input_dir, ead))
+        extents = root_tree.xpath("//extent")
+        extent_list = [[ead, root_tree.getpath(extent), extent.text.encode('utf-8') if extent.text else ""] for extent in extents if extent.get("altrender", "") != "carrier"]
+        extents.append(extent_list)
 
-    with open(path.join(output_dir, "all_extents.csv"), mode="r") as f:
-        reader = csv.reader(f)
-        extent_list = reversed(list(reader))
-        with open(path.join(output_dir, "all_extents_reversed.csv"), mode="wb") as g:
-            writer = csv.writer(g)
-            writer.writerows(extent_list)
+    extents.reverse()
 
+    return extents
 
 if __name__ == "__main__":
     input_dir = r"S:\Curation\Student Work\Walker Boyle\source files\EADs\Master EAD 2015-06-12"
-    write_extents_to_csv(input_dir)
+    get_all_extents(input_dir)
